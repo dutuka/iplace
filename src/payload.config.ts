d57ac3593e362a81faaf55d7ce970eee4b5b509f
dutuka <39162585+dutuka@users.noreply.github.com>
2023-12-06 03:50:38 +0100

    setup admin dashboard

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -2,6 +2,7 @@ import { mongooseAdapter } from "@payloadcms/db-mongodb";
 import { webpackBundler } from "@payloadcms/bundler-webpack";
 import { buildConfig } from "payload/config";
 import { slateEditor } from "@payloadcms/richtext-slate";
+import path from "path";
 
 export default buildConfig({
   serverURL: process.env.NEXT_PUBLIC_SERVER_URL || "",
@@ -17,8 +18,14 @@ export default buildConfig({
       ogImage: "/thumbnail.jpg",
     },
   },
+  rateLimit: {
+    max: 2000,
+  },
   editor: slateEditor({}),
   db: mongooseAdapter({
     url: process.env.MONGODB_URL!,
   }),
+  typescript: {
+    outputFile: path.resolve(__dirname, "payload-types.ts"),
+  },
 });
